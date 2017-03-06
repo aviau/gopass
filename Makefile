debee42b536207f3660322baa6b49837444dd4ab
aviau <alexandre@alexandreviau.net>
2017-03-06 16:32:52 -0500

    Create basic pwgen tests

--- Makefile
+++ Makefile
@@ -6,6 +6,10 @@ SOURCES := $(shell find $(SOURCEDIR) -name '*.go')
 gopass: $(SOURCES)
 	go build -o gopass cmd/gopass/main.go
 
+.PHONY: test
+test:
+	go test -v ./...
+
 .PHONY: install
 install:
 	go install ./...
---
+++ pwgen/pwgen_test.go
@@ -0,0 +1,47 @@
+//   This file is part of gopass.
+//
+//    gopass is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    gopass is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with gopass.  If not, see <http://www.gnu.org/licenses/>.
+
+package pwgen_test
+
+import (
+	"github.com/aviau/gopass/pwgen"
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
+
+func TestLength(t *testing.T) {
+	runes := pwgen.Alpha
+	password := pwgen.RandSeq(10, runes)
+	assert.Equal(t, len(password), 10)
+}
+
+func TestContainsNumsOnly(t *testing.T) {
+	runes := pwgen.Num
+	password := pwgen.RandSeq(50, runes)
+	numsString := string(pwgen.Num)
+	for _, character := range password {
+		assert.True(t, strings.ContainsRune(numsString, character))
+	}
+}
+
+func TestContainsAlphaOnly(t *testing.T) {
+	runes := pwgen.Alpha
+	password := pwgen.RandSeq(50, runes)
+	numsString := string(pwgen.Alpha)
+	for _, character := range password {
+		assert.True(t, strings.ContainsRune(numsString, character))
+	}
+}
