cfbfdddfd865ac8171ba785f1c556e77d5032e1b
aviau <alexandre@alexandreviau.net>
2017-07-26 02:30:51 -0400

    copyright notices: indent first two lines

--- password_store_test.go
+++ password_store_test.go
@@ -1,6 +1,6 @@
-//   Copyright (C) 2017 Alexandre Viau <alexandre@alexandreviau.net>
+//    Copyright (C) 2017 Alexandre Viau <alexandre@alexandreviau.net>
 //
-//   This file is part of gopass.
+//    This file is part of gopass.
 //
 //    gopass is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
