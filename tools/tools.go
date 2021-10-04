4aa2017dc9ee8ae82a54f7a3b4dd00208cb2145b
aviau <alexandre@alexandreviau.net>
2021-10-03 22:08:35 -0400

    tools.go: add empty line

--- tools/tools.go
+++ tools/tools.go
@@ -14,6 +14,7 @@
 //
 //    You should have received a copy of the GNU General Public License
 //    along with gopass.  If not, see <http://www.gnu.org/licenses/>.
+//
 // +build tools
 
 package main
